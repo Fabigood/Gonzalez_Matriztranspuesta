67c4db10bc5613dd85549e59f25707fdc997272a
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-11-23 16:35:32 -0500

    4 paso

--- trans.c
+++ trans.c
@@ -16,24 +16,29 @@ int main()
     printf("la matriz original es: \n");
 
     for (int i = 0; i < n; i++)
-    {
+    {//for para imprimir la matrix normal 
         printf("\n");
 
         for (int j = 0; j < m; j++)
         {
-            Matrix[i][j] = rand() % 100 + 1;
+            Matrix[i][j] = rand() % 100 + 1;        //agregamos para tener numeros randoms
 
             printf("%d\t", Matrix[i][j]);
         }
     }
-printf("\nLa matrix transpues es :\n");
+    printf("\nLa matrix transpues es :\n");
 
-int MatrixTra[m][n]
-for (int )
+    int MatrixTra[m][n];
+     for (int i = 0 ; i < m; i++)
+    {
+        printf("\n");   //se hace li mismo que arriba solo que cambiamos en los valores por lad filas por las columas 
+        for (int j = 0; j < n; j++){
 
+            MatrixTra[i][j]=Matrix[j][i]; //se dice que la matrixtranspuesta se igual a la matrix normal para poder llenarla y no tner el problema del vacio
+            printf("%d\t", MatrixTra[i][j]);
 
-
-
+        }
+    }
 
     return 0;
 }

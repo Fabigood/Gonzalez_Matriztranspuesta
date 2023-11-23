3abfc5ad943dd055308040a3d43f4dbdd7226e91
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-11-23 16:22:29 -0500

    paso 2

--- trans.c
+++ trans.c
@@ -3,16 +3,26 @@
 #include <time.h>
 
 int main(){
+ srand(time(NULL));
  int n, m;
     printf("Ingrese el numero de filas  \n");
     scanf("%d", &n);    //se pide al usuario el tamana de la matrix
     printf("Ingrese el numero de filas  \n");
     scanf("%d", &m);
 
+    int Matrix[n][m];   
+
+    printf("la matriz original es: \n");
+
+    for(int i = 0 ; i<n;i++);{
+        printf("\n");
+
+        for (int j = 0; j<m;j++)
+    }
 
 
 
-    
+
     return 0;
 
 }

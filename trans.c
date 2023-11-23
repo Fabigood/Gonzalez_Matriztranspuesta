3ebb82ba2ce5c4b617cfd7f7195fa9f81e5cc262
Fabigood <fabio.gonzalez.fiallos@udla.edu.ec>
2023-11-23 16:27:55 -0500

    3 paso

--- trans.c
+++ trans.c
@@ -2,27 +2,38 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main(){
- srand(time(NULL));
- int n, m;
+int main()
+{
+    srand(time(NULL));
+    int n, m;
     printf("Ingrese el numero de filas  \n");
-    scanf("%d", &n);    //se pide al usuario el tamana de la matrix
+    scanf("%d", &n); // se pide al usuario el tamana de la matrix
     printf("Ingrese el numero de filas  \n");
     scanf("%d", &m);
 
-    int Matrix[n][m];   
+    int Matrix[n][m];
 
     printf("la matriz original es: \n");
 
-    for(int i = 0 ; i<n;i++);{
+    for (int i = 0; i < n; i++)
+    {
         printf("\n");
 
-        for (int j = 0; j<m;j++)
+        for (int j = 0; j < m; j++)
+        {
+            Matrix[i][j] = rand() % 100 + 1;
+
+            printf("%d\t", Matrix[i][j]);
+        }
     }
+printf("\nLa matrix transpues es :\n");
+
+int MatrixTra[m][n]
+for (int )
+
 
 
 
 
     return 0;
-
 }
